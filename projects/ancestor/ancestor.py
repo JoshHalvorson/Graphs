6c900835571b1c1ca86107856c923cdcd7d18d2d
JoshHalvorson <joshhalvorson16@gmail.com>
2019-10-09 09:28:28 -0700

    Add graph solution
    Forgot we were in graph week and solved it originally like a hacker rank challenge lol

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -24,6 +24,68 @@ def earliest_ancestor(ancestors, starting_node, visited=None):
             return earliest_ancestor(ancestors, pair[0], visited)
     return starting_node
 
+
+# class Queue:
+#     def __init__(self):
+#         self.queue = []
+#
+#     def enqueue(self, value):
+#         self.queue.append(value)
+#
+#     def dequeue(self):
+#         if self.size() > 0:
+#             return self.queue.pop(0)
+#         else:
+#             return None
+#
+#     def size(self):
+#         return len(self.queue)
+#
+#
+# class Graph:
+#     def __init__(self):
+#         self.vertices = {}
+#
+#     def add_vertex(self, vertex_id):
+#         if vertex_id not in self.vertices:
+#             self.vertices[vertex_id] = set()
+#
+#     def add_edges(self, v1, v2):
+#         if v1 in self.vertices and v2 in self.vertices:
+#             self.vertices[v1].add(v2)
+#         else:
+#             raise KeyError('Vertex does not exist')
+#
+#
+# def earliest_ancestor(ancestors, starting_node):
+#     graph = Graph()
+#
+#     for pair in ancestors:
+#         graph.add_vertex(pair[0])
+#         graph.add_vertex(pair[1])
+#
+#         graph.add_edges(pair[1], pair[0])
+#
+#     q = Queue()
+#     q.enqueue([starting_node])
+#     max_path_length = 1
+#     earliest_ancestor = -1
+#
+#     while q.size() > 0:
+#         path = q.dequeue()
+#         vertex = path[-1]
+#
+#         if(len(path) >= max_path_length and vertex < earliest_ancestor) or (len(path) > max_path_length):
+#             earliest_ancestor = vertex
+#             max_path_length = len(path)
+#
+#         for neighbor in graph.vertices[vertex]:
+#             new_path = list(path)
+#             new_path.append(neighbor)
+#             q.enqueue(new_path)
+#     return earliest_ancestor
+
+
 if __name__ == '__main__':
     test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
     print(earliest_ancestor(test_ancestors, 9))
